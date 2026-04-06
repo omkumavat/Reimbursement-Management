528a7827e436db292bdf12ef137934c6cf49cfd7
omkumava <omkumavat2004@gmail.com>
2026-04-06 16:47:16 +0530

    done

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://reimbursement-management-system-rosy.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 
 // Make uploads folder static
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
